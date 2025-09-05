c9ec6bd309f7e9264a84e6d2ae233822a64de852
Premal007 <premalpatel.abc@gmail.com>
2025-09-05 11:43:35 +0530

    navbar

--- frontend/interview-prep-ai/src/components/layouts/Navbar.jsx
+++ frontend/interview-prep-ai/src/components/layouts/Navbar.jsx
@@ -5,7 +5,7 @@ import ProfileInfoCard from '../Cards/ProfileInfoCard'
 function Navbar() {
   return (
     <div className='h-16  bg-white border border-b border-gray-200/50 backdrop-blur-[2px] py-2.5 px-4 md:px-0 sticky top-0 z-30'>
-      <div className='container mx-auto flex items-center justify-between gap-5'>
+      <div className='container mx-auto flex items-center justify-between gap-5 max-w-screen-xl'>
         <Link to="/dashboard">
           <h2 className='text-lg md:text-xl font-medium text-black leading-5 '>
             Interview Prep AI
